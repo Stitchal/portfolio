5f0957091c2fe465da98dddd3677b7664b564fec
Alexis <alexis.rosset06@gmail.com>
2024-06-01 15:35:02 +0200

    maj de l'année du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="flex items-center justify-center bg-gradient-to-r from-background-color to-container-bg text-custom-white ">
       <div className="flex items-center justify-center w-full gap-2 py-8 border-blue-500 border-t 2xl:w-2/3 flex-col mx-4 lg:px-0">
-        <p>Développé par Alexis Rosset - Tous droits réservés © - 2023</p>
+        <p>Développé par Alexis Rosset - Tous droits réservés © - 2024</p>
         <div className="flex flex-row gap-2">
           <a
             href="https://www.linkedin.com/in/alexisrosset/"
