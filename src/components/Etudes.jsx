d393d087784e71cc2df2ae32b3ba84622572b60e
Alexis <alexis.rosset06@gmail.com>
2025-03-15 11:35:20 +0100

    changed photo & cursus

--- src/components/Etudes.jsx
+++ src/components/Etudes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "./Container";
 import iutImage from "../assets/iut.png";
-import universiteCoteDazurImage from "../assets/universiteCoteDazur.png";
+import miageImage from "../assets/miage.png";
 import EtudeItem from "./EtudeItem";
 
 const Etudes = () => {
@@ -11,11 +11,19 @@ const Etudes = () => {
         <EtudeItem
           link="https://univ-cotedazur.fr/miage"
           title="MIAGE de Nice Sophia Antipolis - Université Côte d'Azur"
-          name="Licence 3 MIAGE"
-          year="2023 - 2024"
-          image={universiteCoteDazurImage}
+          name="Master 1 MIAGE"
+          year="2024"
+          image={miageImage}
           location="https://www.google.com/maps?sca_esv=579280459&rlz=1C1ONGR_frFR1044FR1044&output=search&q=miage+nice&source=lnms&entry=mc&sa=X&ved=2ahUKEwj0po_n46iCAxV3UaQEHdwxAcwQ0pQJegQICRAB"
           actual
+        />
+        <EtudeItem
+          link="https://univ-cotedazur.fr/miage"
+          title="MIAGE de Nice Sophia Antipolis - Université Côte d'Azur"
+          name="Licence 3 MIAGE"
+          year="2023 - 2024"
+          image={miageImage}
+          location="https://www.google.com/maps?sca_esv=579280459&rlz=1C1ONGR_frFR1044FR1044&output=search&q=miage+nice&source=lnms&entry=mc&sa=X&ved=2ahUKEwj0po_n46iCAxV3UaQEHdwxAcwQ0pQJegQICRAB"
         />
         <EtudeItem
           link="https://iut.univ-cotedazur.fr/"
